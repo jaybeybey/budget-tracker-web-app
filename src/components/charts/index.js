33db14673bfbfe4fd0520b3693cc990c5ade9c98
teodorico <jayaquitania@gmail.com>
2019-10-06 23:13:58 +0200

    refactored some css

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import './styles.css'
+import logo from './save-money.png'
 
 import BalanceChart from './balanceChart'
 import SpendingChart from './spendingChart'
@@ -29,12 +30,11 @@ const mapDispatchMontlyDate = { onCurrentMonthChange: setCurrentBudgetMonth }
 const ConnectedMontlyDate = connect(mapStateMontlyDate, mapDispatchMontlyDate)(MontlyDate);
 
 const Report = (props) => {
-    const { income, expenses } = props;
+    const { income, expenses, currency } = props;
     const totalSpending = expenses.map(num => parseInt(num.amount));
     const reducedTotalSpending = totalSpending.reduce((a, c) => a + c, 0);
     const totalSaving = income - reducedTotalSpending;
     const dataHandler = expenses.length > 0;
-
     const renderCharts = () => {
         return dataHandler ?
             <>
@@ -42,7 +42,10 @@ const Report = (props) => {
                     <div className="col-md-12 col-lg-3">
                         <SpendingChart totalSpending={reducedTotalSpending} income={income} />
                     </div>
-                    <div className="col-md-12 col-lg-3">
+                    <div className="col-md-12 col-lg-3 savings">
+                        <img src={logo} alt='Logo' />
+                        <h2>Savings</h2>
+                        <h5>{totalSaving} <span>{currency}</span></h5>
                     </div>
                     <div className="col-md-12 col-lg-3">
                         <BalanceChart totalSaving={totalSaving} income={income} />
@@ -81,7 +84,8 @@ const mapStateToProps = state => {
     return {
         income: state.user.income,
         expenses: getCurrentMonthExpenses(state),
-        savings: state.newSavings
+        savings: state.newSavings,
+        currency: state.user.defaultCurrency
     }
 }
 
